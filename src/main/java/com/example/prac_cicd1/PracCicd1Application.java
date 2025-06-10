089d116a0f08bb44f1915c78098483cf94ae8904
yoomeanthat <yoomeani1214@gmail.com>
2025-06-10 09:42:58 +0900

    cicd1

---
+++ src/main/java/com/example/prac_cicd1/PracCicd1Application.java
@@ -0,0 +1,13 @@
+package com.example.prac_cicd1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracCicd1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracCicd1Application.class, args);
+	}
+
+}
